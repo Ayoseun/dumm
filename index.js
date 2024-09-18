8e166dfef6bed4c7516c8838eea555716b8e44d6
Ayo Solomon <48213053+Ayoseun@users.noreply.github.com>
2024-09-18 13:37:36 +0100

    .

--- index.js
+++ index.js
@@ -12,17 +12,34 @@ app.use(cors());
 app.use(express.json());
 
 // Route for ping
-app.get('/', (req, res) => {
+app.get('/ping', (req, res) => {
   res.send('Pong');
 });
 
-// Route to make the API call
+// Route to initiate payment
 app.post('/initiate-payment', async (req, res) => {
   try {
     const paymentData = req.body;  // Get the request body
 
     const response = await axios.post(
       'https://card-payment-00001-fxn-t7xgy74vla-uk.a.run.app/api/v1/cardpayment/initiate',
+      paymentData
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error initiating payment');
+  }
+});
+
+// New route to process card payment
+app.post('/process-payment', async (req, res) => {
+  try {
+    const paymentData = req.body;  // Get the request body
+
+    const response = await axios.post(
+      'https://card-payment-00001-fxn-t7xgy74vla-uk.a.run.app/api/v1/cardpayment/pay',
       paymentData
     );
 
