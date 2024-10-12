4a4277b77f85d046a5264237d4d5f51b52bf0a14
Ayo Solomon <48213053+Ayoseun@users.noreply.github.com>
2024-10-12 04:17:20 +0100

    .

--- index.js
+++ index.js
@@ -10,19 +10,35 @@ app.use(cors());
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
-
+const baseurl = "https://orokii-ppg-gateway-api-730399970440.us-central1.run.app/api/v1"
 // Route for ping
 app.get('/ping', (req, res) => {
   res.send('Pong');
 });
 
 // Route to initiate payment
-app.post('/initiate-payment', async (req, res) => {
+app.post('/payment/simple-card-tokenized', async (req, res) => {
   try {
     const paymentData = req.body;  // Get the request body
 
     const response = await axios.post(
-      'https://card-payment-00001-fxn-t7xgy74vla-uk.a.run.app/api/v1/cardpayment/initiate',
+      baseurl+'/payment/simple-card-tokenized',
+      paymentData
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error initiating payment');
+  }
+});
+
+app.post('/payment/tokenized-payment', async (req, res) => {
+  try {
+    const paymentData = req.body;  // Get the request body
+
+    const response = await axios.post(
+      baseurl+'/payment/tokenized-payment',
       paymentData
     );
 
@@ -34,12 +50,28 @@ app.post('/initiate-payment', async (req, res) => {
 });
 
 // New route to process card payment
-app.post('/process-payment', async (req, res) => {
+app.post('/payment/payment-ach', async (req, res) => {
   try {
     const paymentData = req.body;  // Get the request body
 
     const response = await axios.post(
-      'https://card-payment-00001-fxn-t7xgy74vla-uk.a.run.app/api/v1/cardpayment/pay',
+      baseurl+'/payment/payment-ach',
+      paymentData
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error processing payment');
+  }
+});
+
+app.post('/payment/payment-ach-token-id', async (req, res) => {
+  try {
+    const paymentData = req.body;  // Get the request body
+
+    const response = await axios.post(
+      baseurl+'/payment/payment-ach-token-id',
       paymentData
     );
 
